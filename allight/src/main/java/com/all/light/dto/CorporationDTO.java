70355839c989fa1ee79a8fe2fe0acecc25791734
김예림 <glad0779@naver.com>
2020-07-01 14:23:12 +0900

    1

---
+++ allight/src/main/java/com/all/light/dto/CorporationDTO.java
@@ -0,0 +1,20 @@
+package com.all.light.dto;
+
+public class CorporationDTO {
+	private String coid;
+	private String copw;
+	
+	public String getCoid() {
+		return coid;
+	}
+	public void setCoid(String coid) {
+		this.coid = coid;
+	}
+	public String getCopw() {
+		return copw;
+	}
+	public void setCopw(String copw) {
+		this.copw = copw;
+	}
+	
+}

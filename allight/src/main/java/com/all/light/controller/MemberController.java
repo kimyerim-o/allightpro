3c390a5221a1579cdd7ee8c3a5d029377e462298
salixryu <ryu@dxp.kr>
2020-07-17 22:18:01 +0900

    0717
    0717

--- allight/src/main/java/com/all/light/controller/MemberController.java
+++ allight/src/main/java/com/all/light/controller/MemberController.java
@@ -2,7 +2,6 @@ package com.all.light.controller;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;

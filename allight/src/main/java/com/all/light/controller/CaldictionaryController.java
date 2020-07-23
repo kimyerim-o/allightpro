6e08b5bcb0c181ba38f0dbf8dabf3e6e6929038a
salixryu <ryu@dxp.kr>
2020-07-23 19:37:55 +0900

    0723
    0723

--- allight/src/main/java/com/all/light/controller/CaldictionaryController.java
+++ allight/src/main/java/com/all/light/controller/CaldictionaryController.java
@@ -1,12 +1,17 @@
 package com.all.light.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.all.light.dto.CaldictionaryDTO;
 import com.all.light.service.CaldictionaryService;
+import com.all.light.util.PageUtil;
 
 @Controller
 @RequestMapping("/cal")
@@ -14,26 +19,45 @@ public class CaldictionaryController {
 	
 	@Autowired
 	CaldictionaryService calSVC;
+
 	
-<<<<<<< HEAD
-=======
+	@GetMapping("/dictionary")
+	public String callist() {
+		System.out.println("callist 폼 진입");
+		return "/diary/admin/dictionary/list";
+	}
+
 	
-	// 사전 보기
->>>>>>> 185dc686f85f2776ccd54bcb307c70a6ca492118
-	@RequestMapping("/dictionary")
-	public ModelAndView calview(
+	// 음식 사전 보기
+	@RequestMapping("/dictionary/food")
+	public ModelAndView foodview(
 			@RequestParam(value="nowPage", required=false, defaultValue="1") int nowPage,
 			@RequestParam(value = "searchWord", required = false, defaultValue="") String searchWord,
 			ModelAndView mv){
-<<<<<<< HEAD
-		
-		
-		
-=======
-		System.out.println("컨트롤러 상품 목록보기 - listView() 요청");
->>>>>>> 185dc686f85f2776ccd54bcb307c70a6ca492118
-		
-		return null;
+
+		System.out.println("컨트롤러 상품 목록보기 - foodview() 요청");
+		PageUtil pInfo = calSVC.getPageInfo(nowPage, searchWord);
+		System.out.println("컨트롤러 상품 목록보기 - pInfo : " + pInfo);
+		ArrayList<CaldictionaryDTO> list = calSVC.getListView(pInfo, searchWord);
+		mv.addObject("LIST", list);		// 실제 조회 목록
+		mv.addObject("PINFO", pInfo);	// 페이징 관련 정보
+		mv.addObject("searchWord", searchWord);	// 페이징 관련 정보
+		mv.setViewName("/diary/admin/dictionary/list");
+		System.out.println("컨트롤러 상품 목록보기 - list = " + list);
+		System.out.println("컨트롤러 상품 목록보기 - pInfo = " + pInfo);
+		System.out.println("컨트롤러 상품 목록보기 - searchWord = " + searchWord);
+		System.out.println("컨트롤러 상품 목록보기 - mv = " + mv);
+		return mv;
 	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
